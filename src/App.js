09ab65038e0a96380cbb7e3d823e391daebdcc7c
Prem Vikas <premvikas@Prems-MacBook-Air.local>
2023-10-16 11:59:02 +0530

    adding toggle remainder highlight

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ const [tasks, setTasks] = useState(
           id:1,
           text: 'Sprint Discussion',
           day: 'day 1',
-          remainder: false
+          remainder: true
       },
       {
           id:2,
           text: 'Deep Work',
           day: 'day 2',
-          remainder: false
+          remainder: true
       },
       {
           id:3,
@@ -35,11 +35,20 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id));
 }
 
+const toggleRemainder = (id) => {
+  console.log("yo", id);
+  setTasks(
+    tasks.map((task) => 
+      task.id === id ? { ...task, remainder : !task.remainder} : task
+    )
+  )
+}
+
   return (
     <div className="App">
       <h1>React crash course</h1>
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No Tasks To Show'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleRemainder}/> : 'No Tasks To Show'}
     </div>
   );
 }
